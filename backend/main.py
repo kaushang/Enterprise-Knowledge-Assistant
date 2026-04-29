d8014d3b5c5e814cad12d85ddbac2d1524e9bc95
Kaushang Surya <kaushangsurya29@gmail.com>
2026-04-29 12:37:43 +0530

    Refactor: set CORS policy in backend

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Enterprise Knowledge Assistant")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174", "https://enterprise-knowledge-assistant-kappa.vercel.app/"],
+    allow_origins=["http://localhost:5174", "https://enterprise-knowledge-assistant-kappa.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
